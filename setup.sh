c6a829eb9dabace90cdc60a20fd39035bd97ecb4
ezhang7423 <ezhang7423@student.palomar.edu>
2021-11-26 11:08:53 -0800

    ok

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# You'll have to have mujoco 210 installed beforehand
+
+python3 -m venv venv
+source venv/bin/activate
+git clone https://github.com/HorizonRobotics/alf
+cd alf
+pip install -e .
+mkdir logs
+pip3 install -U 'mujoco-py<2.2,>=2.1'
+python -m alf.bin.train --conf=oac_humanoid_conf.py --root_dir=logs
